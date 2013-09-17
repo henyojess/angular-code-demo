9f5285b50c96b49f666021c2334bf6c073812ccc
jessie <jessie@Aiza-Llapitans-MacBook-Pro.local>
2013-09-17 23:41:28 +0800

    Code Demo#4:Display a list of tasks
    * removed the name definition in $scope
    * defined an array of tasks within the $scope of DemoController
    * replaced div content with <table> markup
    * added ng-repeat directive to <tr> tag
    * added binding to display task description and completed

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,8 @@
 function DemoController($scope){
-    $scope.name = 'World';
+    $scope.tasks = [
+        {description: 'first entry',completed:false},
+        {description: 'another entry',completed:false},
+        {description: 'some other entry',completed:false},
+        {description: 'last entry',completed:false}
+    ];
 }
